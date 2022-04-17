e532eb01c6cec843626f689a604f0ee4cd66f492
jindong <1229303219@qq.com>
2022-04-17 21:50:57 +0800

    chore: add typeof record

---
+++ docs/typeof-操作符.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description: 描述
+ * @Autor: jind
+ * @Date: 2022-04-17 21:49:19
+ * @LastEditors: jind
+ * @LastEditTime: 2022-04-17 21:50:26
+ */
+
+let S = 'hello'
+
+// n:string 类型
+let N: typeof S
