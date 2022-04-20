b59ff58043212a3fe8ae828262d65e105864cf09
jindong <1229303219@qq.com>
2022-04-20 08:02:33 +0800

    chore: easy push

--- type-challenges/3057-easy-push/template.ts
+++ type-challenges/3057-easy-push/template.ts
@@ -3,19 +3,14 @@
  * @Autor: jind
  * @Date: 2022-04-08 22:36:04
  * @LastEditors: jind
- * @LastEditTime: 2022-04-08 22:45:46
+ * @LastEditTime: 2022-04-20 08:00:58
  */
-type Push<T, U> = {
-  T: T['length'] = U
-}
+type Push<T extends any[], U> = [...T, U]
 
 function myPush(arr: any[], key: any) {
   let temp = []
   arr.push(key)
 }
 
-type Q = [1]
-
-type rr = keyof { name: string }
-
 type Result = Push<[1, 2], '3'> // [1, 2, '3']
+type R2 = Push<['1', 2, '3'], boolean>
